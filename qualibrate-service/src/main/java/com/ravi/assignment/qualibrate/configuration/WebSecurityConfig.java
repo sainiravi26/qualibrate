0e702b3a272a6476d4d9673d065ba2219f54d1ff
Ravi Saini <sainiravi26@gmail.com>
2019-12-08 22:49:16 +0100

    fix security + add tests

--- qualibrate-service/src/main/java/com/ravi/assignment/qualibrate/configuration/WebSecurityConfig.java
+++ qualibrate-service/src/main/java/com/ravi/assignment/qualibrate/configuration/WebSecurityConfig.java
@@ -22,6 +22,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/").permitAll();
         http.cors().and();
     }
 
